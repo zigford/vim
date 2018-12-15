5844e95a1c42295f48b33474f1221aa7f036d8da
Jesse Harris <zigford@gmail.com>
2018-12-16 09:01:20 +1000

    Link vimrc rather than copy

--- scripts/setupvim.sh
+++ scripts/setupvim.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Function to get full path to a relative file. POSIX compliant. Ripped from:
+# https://stackoverflow.com/a/3915420
+
+function gfilepath(){
+	echo "$(cd "$(dirname "$1")"; pwd -P)/$(basename "$1")"
+}
+
 #deps
 which git || sudo apt-get install git -y
 
@@ -13,4 +20,4 @@ if ! test -f ~/.vim/autoload/plug.vim; then
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
-cp -f ../.vimrc ~/.vimrc
+ln -s $(gfilepath ../.vimrc) ~/.vimrc
